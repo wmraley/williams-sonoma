febd616aa60d6019085748b8e956a046c9634ed7
Mike Raley <brco@mindspring.com>
2017-06-22 19:50:48 -0400

    Adding AppBasics helper, first commit in basic-project

--- src/main/java/com/sqa/mr/helpers/AppBasics.java
+++ src/main/java/com/sqa/mr/helpers/AppBasics.java
@@ -9,6 +9,8 @@ package com.sqa.mr.helpers;
 
 import java.util.*;
 
+import com.sqa.mr.helpers.exceptions.*;
+
 /**
  * AppBasics //ADDD (description of class)
  * <p>
@@ -38,5 +40,95 @@ public class AppBasics {
 		System.out.println("Welcome to the " + appName + " Application.");
 		System.out.print("Could I get your name please: ");
 		return scanner.nextLine();
+	}
+
+	public static boolean requestBoolean(String question) {
+		System.out.println(question);
+		return false;
+	}
+
+	public static char requestChar(String question) {
+		boolean isInvalid = true;
+		String input = "";
+		while (isInvalid) {
+			System.out.println(question + " ");
+			input = scanner.nextLine();
+			try {
+				if (input.length() > 1) {
+					throw new InvalidCharResponseLength();
+				}
+				isInvalid = false;
+			} catch (InvalidCharResponseLength e) {
+				// TODO Auto-generated catch block
+				System.out.println("You have entered too many chars [" + input + "]");
+			}
+		}
+		return input.charAt(0);
+	}
+
+	public static double requestDouble(String question) {
+		double value = 0;
+		boolean isInvalid = true;
+		while (isInvalid) {
+			System.out.print(question + " ");
+			String input = scanner.nextLine();
+			try {
+				value = Double.parseDouble(input.trim());
+				isInvalid = false;
+			} catch (NumberFormatException e) {
+				System.out.println("YOu have not entered a correct formatted number [" + input + "]");
+				// TODO: handle exception
+			}
+		}
+		return value;
+	}
+
+	public static float requestFloat(String question) {
+		float value = 0;
+		boolean isInvalid = true;
+		while (isInvalid) {
+			System.out.print(question + " ");
+			String input = scanner.nextLine();
+			try {
+				value = Float.parseFloat(input.trim());
+				isInvalid = false;
+			} catch (NumberFormatException e) {
+				System.out.println("YOu have not entered a correct decimal formatted number [" + input + "]");
+				// TODO: handle exception
+			}
+		}
+		return value;
+	}
+
+	public static int requestInt(String question) {
+		int value = 0;
+		boolean isInvalid = true;
+		while (isInvalid) {
+			System.out.print(question + " ");
+			String input = scanner.nextLine();
+			try {
+				value = Integer.parseInt(input.trim());
+				isInvalid = false;
+			} catch (NumberFormatException e) {
+				System.out.println("YOu have not entered a correct formatted number [" + input + "]");
+				// TODO: handle exception
+			}
+		}
+		return value;
+	}
+
+	public static long requestLong(String question) {
+		System.out.println(question);
+		return 0;
+	}
+
+	public static short requestShort(String question) {
+		System.out.println(question);
+		return 0;
+	}
+
+	public static String requestString(String question) {
+		System.out.println(question);
+		return scanner.nextLine().trim();
 	}
 }
